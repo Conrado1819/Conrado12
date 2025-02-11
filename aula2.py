5c99391250630eff563d10a83c3c8890fb4785bb
Conrado1819 <conradoottoharms@gmail.com>
2025-02-10 22:58:34 -0300

    aula2.py

---
+++ aula2.py
@@ -0,0 +1,12 @@
+nome = 'conrado'
+sobrenome = 'harms'
+idade = 17
+ano_nascimento = 2007
+maior_de_idade = idade >=18
+altura_metros = 1.94
+print ('Nome:',nome)
+print ('Sobrenome:', sobrenome)
+print ('Idade:' , idade)
+print ('Ano_nascimento :' , ano_nascimento)
+print ('Maior_de_idade:', maior_de_idade )
+print ('Altura_metros:' , altura_metros)
