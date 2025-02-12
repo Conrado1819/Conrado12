60ac6ad427f9cda6e39ef3c414c4cbf83b063c6c
Conrado1819 <conradoottoharms@gmail.com>
2025-02-11 23:14:57 -0300

    aula3.py

---
+++ aula3.py
@@ -0,0 +1,9 @@
+multiplicacao = 10 * 5
+print ('multiplicacao', multiplicacao )
+divisao = 10 / 6
+print ('divisao', divisao)
+exponenciacao = 2 ** 5
+print ('exponenciacao', exponenciacao)
+print ('exponenciacao', exponenciacao == 32)
+print ('multiplicacao', multiplicacao <= 30)
+print ('multiplicacao', multiplicacao >= 50)
